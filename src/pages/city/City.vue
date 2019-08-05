03567423dec61db1ea70f4d96e8ba436bfb79b17
lcc908 <593261887@qq.com>
2019-08-05 20:10:00 +0800

    城市静态页

---
+++ src/pages/city/City.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <CityHeader></CityHeader>
+    </div>
+  <!--<city-header></city-header>-->
+</template>
+
+<script>
+  import CityHeader from './components/Header'
+export default {
+    name: "City",
+  components:{
+    CityHeader
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/home' //@ 指src目录
+import City from '@/pages/city/City' //@ 指src目录
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/', //当用户访问根路径的时候  加载HelloWorld这个组件的内容
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/city', //当用户访问根路径的时候  加载HelloWorld这个组件的内容
+      name: 'City',
+      component: City
     }
   ]
 })
