d76f82e690b8089f612d458de46469f3f9400a1e
lcc908 <593261887@qq.com>
2019-08-19 20:06:14 +0800

    zhushi

--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -1,19 +1,24 @@
 <template>
     <div>
       <detailbanner></detailbanner>
+      <DetailHeader></DetailHeader>
+      <div class="content"></div>
     </div>
 </template>
 
 <script>
-  import detailbanner from './components/Banner'
-    export default {
-        name: "detail",
-      components:{
-        detailbanner
-      }
+import detailbanner from './components/Banner'
+import DetailHeader from './components/Header'
+  export default {
+    name: "detail",
+    components:{
+      detailbanner,
+      DetailHeader
     }
+  }
 </script>
 
 <style scoped lang="stylus">
-
+.content
+  height: 50rem
 </style>
--- src/pages/home/components/recomed.vue
+++ src/pages/home/components/recomed.vue
@@ -3,7 +3,9 @@
     <div class="title">热销推荐</div>
     <ul>
       <!--<router-link to="/detail">-->
-        <router-link :to="'/detail/' + item.id" class="item border-bottom" v-for="item of recomedList1" :key="item.id">
+        <router-link
+          tag="li"
+          :to="'/detail/' + item.id" class="item border-bottom" v-for="item of recomedList1" :key="item.id">
           <img class="item-img" :src="item.imgUrl" />
           <div class="item-info">
             <p class="item-title">{{item.title}}</p>
